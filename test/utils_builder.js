e304f57ffa064a0b8663756311be08d291247024
decaffeinate <k@maxlath.eu>
2019-12-01 20:01:42 +0100

    decaffeinate: Run post-processing cleanups on inv-utils.coffee and 5 other files

--- test/utils_builder.js
+++ test/utils_builder.js
@@ -1,3 +1,5 @@
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
 const _ = require('underscore');
 const loggers_ = require('inv-loggers');
 module.exports = _.extend(_, loggers_, require('../src/inv-utils')(_));
